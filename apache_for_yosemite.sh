03b77d35e4dc94546caf77294f4251eedfcdec92
binarylu <luxiakun@gmail.com>
2015-09-13 01:35:41 -0400

    add install script

---
+++ apache_for_yosemite.sh
@@ -0,0 +1,61 @@
+#/bin/sh
+
+set -e
+
+source /etc/profile
+
+username=`whoami`
+
+sudo echo "Step 1: Create ~/Sites"
+if [ -d ~/Sites ]; then
+    read -p "The directory 'Site' exists, override it? (y/n) " yon
+    if [ $yon = "n" -o $yon = "N" ]; then
+        echo "exit"
+        exit 0
+    fi
+fi
+sudo rm -fr ~/Sites
+mkdir ~/Sites
+echo "Hooray! My site works!" > ~/Sites/index.html.en
+cat >> ~/Sites/index.php << EOF
+<?php echo phpinfo(); ?>
+EOF
+
+echo "Step 2: Modify httpd.conf"
+sudo sed -Ei "" 's/^#(.*php5_module.*)/\1/g' /etc/apache2/httpd.conf
+sudo sed -Ei "" 's/^#(.*userdir_module.*)/\1/g' /etc/apache2/httpd.conf
+sudo sed -Ei "" 's/^#(.*httpd-userdir.*)/\1/g' /etc/apache2/httpd.conf
+
+echo "Step 3: Modify httpd-userdir.conf"
+sudo sed -Ei "" 's/^#(Include.*apache2.*users.*)/\1/g' /private/etc/apache2/extra/httpd-userdir.conf
+
+echo "Step 4: Create /etc/apache2/users/$username.conf"
+if [ -f /etc/apache2/users/$username.conf ]; then
+    read -p "'/etc/apache2/users/$username.conf' exists, override it? (y/n) " yon
+    if [ $yon = "n" -o $yon = "N" ]; then
+        echo "exit"
+        exit 0
+    fi
+fi
+sudo rm -fr /etc/apache2/users/$username.conf
+sudo sh -c 'cat >> /etc/apache2/users/'$username'.conf << EOF
+<Directory "/Users/'$username'/Sites/">
+    AddLanguage en .en
+    LanguagePriority en fr de
+    ForceLanguagePriority Fallback
+    Options Indexes MultiViews
+    AllowOverride None
+    Order allow,deny
+    Allow from localhost
+    Require all granted
+</Directory>
+EOF'
+
+echo "Step 5: restart apache"
+sudo apachectl restart
+
+echo "Successful!"
+echo "Test these three URLs in your browser:"
+echo "http://localhost"
+echo "http://localhost/~$username"
+echo "http://localhost/~$username/index.php"
--- install.sh
+++ install.sh
@@ -1,24 +1,18 @@
 set -e
 
+source /etc/profile
+
 [ -f "/usr/bin/sw_vers" ] && os="mac" || os="linux"
 
-if [ $os = "mac" ]; then
-    COLOR_RED=$(    echo "\033[31;1m" )
-    COLOR_GREEN=$(  echo "\033[32;1m" )
-    COLOR_YELLOW=$( echo "\033[33;1m" )
-    COLOR_BLUE=$(   echo "\033[34;1m" )
-    COLOR_MAGENTA=$(echo "\033[35;1m" )
-    COLOR_CYAN=$(   echo "\033[36;1m" )
-    COLOR_RESET=$(  echo "\033[0m"    )
-else
-    COLOR_RED=$(    echo -e "\033[31;1m" )
-    COLOR_GREEN=$(  echo -e "\033[32;1m" )
-    COLOR_YELLOW=$( echo -e "\033[33;1m" )
-    COLOR_BLUE=$(   echo -e "\033[34;1m" )
-    COLOR_MAGENTA=$(echo -e "\033[35;1m" )
-    COLOR_CYAN=$(   echo -e "\033[36;1m" )
-    COLOR_RESET=$(  echo -e "\033[0m"    )
-fi
+[ $os = "mac" ] && echoflag="" || echoflag="-e"
+COLOR_RED=$(    eval echo $echoflag "'\033[31;1m'" )
+COLOR_GREEN=$(  eval echo $echoflag "'\033[32;1m'" )
+COLOR_YELLOW=$( eval echo $echoflag "'\033[33;1m'" )
+COLOR_BLUE=$(   eval echo $echoflag "'\033[34;1m'" )
+COLOR_MAGENTA=$(eval echo $echoflag "'\033[35;1m'" )
+COLOR_CYAN=$(   eval echo $echoflag "'\033[36;1m'" )
+COLOR_RESET=$(  eval echo $echoflag "'\033[0m'"    )
+unset echoflag
 
 if [ ! -n "$MYDOT" ]; then
     MYDOT=~/.dotfiles
@@ -31,11 +25,12 @@ fi
 
 doIt() {
     filename=~/.$1
+    bakdir=~/.dotfiles.bak
     if [ -f $filename ] || [ -h $filename ]; then
-        echo "${COLOR_RED}Found ${filename}.${COLOR_RESET} Backing up to $MYDOT/.${1}.bak.";
-        mv $filename $MYDOT/.${1}.bak
+        echo "${COLOR_RED}Found ${filename}.${COLOR_RESET} Backing up to ${bakdir}/.${1}.bak.";
+        mv $filename ${bakdir}/.${1}.bak
     fi
-    cp -r $MYDOT/$1 $filename
+    cp -r ${bakdir}/$1 $filename
 }
 
 install() {
@@ -53,15 +48,42 @@ hash git >/dev/null 2>&1 && env git clone --depth=1 https://github.com/binarylu/
     exit
 }
 
+## config bash
+#TODO mac bashrc.mac & linux bashrc.linux
+
+if [ "$os" = "mac" ]; then
+    install bash_profile
+    mv ${MYDOT}/bashrc.mac ${MYDOT}/bashrc
+else
+    mv ${MYDOT}/bashrc.linux ${MYDOT}/bashrc
+fi
 install bashrc
 
-[ "$os" = "mac" ] && install bash_profile
+## config zsh
+CHECK_ZSH_INSTALLED=$(grep /zsh$ /etc/shells | wc -l)
+if [ ! $CHECK_ZSH_INSTALLED -ge 1 ]; then
+    echo "${COLOR_YELLOW}Zsh is not installed, pass!${COLOR_RESET}"
+else
+    install zshrc
+fi
+unset CHECK_ZSH_INSTALLED
 
-install zshrc
-
-install vimrc
-
+## config git
 install gitconfig
+
+## config vim
+install vimrc
+install vim
+echo "${COLOR_GREEN}"
+read -p "Install vim plugin? (y/n) " -n 1;
+echo "${COLOR_RESET}"
+if [ $REPLY = "Y" ] || [ $REPLY = "y" ]; then
+    rm -fr ~/.vim/autoload/pathogen.vim
+    rm -fr ~/.vim/bundle/Vundle.vim
+    curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    vim +PluginInstall +qall
+fi
 
 TEST_CURRENT_SHELL=$(expr "$SHELL" : '.*/\(.*\)')
 if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
@@ -69,3 +91,14 @@ if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
 elif [ "$TEST_CURRENT_SHELL" != "bash" ]; then
     . ~/.bashrc
 fi
+
+if [ "$os" = "mac" ]; then
+    echo "${COLOR_GREEN}"
+    read -p "Your OS is Mac OS, need to config webserver? (y/n) " -n 1;
+    echo "${COLOR_RESET}"
+    if [ $REPLY = "Y" ] || [ $REPLY = "y" ]; then
+        ${MYDOT}/apache_for_yosemite.sh
+    fi
+fi
+
+rm -fr $MYDOT
